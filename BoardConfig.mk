0ddab8f3b3882981602280bd1f89919d88003bd7
andi34 <skate4life@gmx.de>
2014-07-21 00:21:24 +0200

    use cyanogenmod_defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/lt01wifi/bluetooth
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4x12
-TARGET_KERNEL_CONFIG := custom_lt01wifi_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_lt01wifi_defconfig
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := lt01wifi,lt01wifixx,lt01wifiue,SM-T310
